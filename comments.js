d4594952cb38ade73ec8bc1417ba58c62f6c00a2
Vignesh Kennadi <vignesh.kennadi@thoughtworks.com>
2024-11-03 07:13:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,26 @@
 // Create Web Server
 // Create a web server that listens on port 3000 and serves the comments.html file
 // when someone visits http://localhost:3000/comments.
-// Use the fs module to read the comments.html file and serve it back to the user.
-// Use the http module to create the server.
 
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+// This means you will need to create a comments.html file in your project directory.
+// The comments.html file should have an h1 tag that says "Comments" and a ul tag
+// with at least 5 li tags with comments in them.
 
-const server = http.createServer((req, res) => {
-    console.log(`Request made to ${req.url}`);
+// When you visit http://localhost:3000/comments in your browser, you should see the
+// comments page.
 
-    if (req.url === '/comments') {
-        fs.readFile(path.join(__dirname));}
-    });
+// Make sure you are using the fs module to read the contents of the comments.html
+// file.
+
+// You can use the following code to read the contents of the comments.html file:
+
+// fs.readFile('./comments.html', 'utf8', (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// });
+// You can use the following code to serve the contents of the comments.html file:
+
+// http.createServer((req, res) => {
+//   fs.readFile('./comments.html', 'utf8'
